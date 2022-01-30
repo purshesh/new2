0e817cee0c03252e5b3add0e98c2744bcfc82610
purshesh <84610269+purshesh@users.noreply.github.com>
2022-01-30 15:42:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+status = "" ; 
+
+function perload() { 
+
+ 
+
+} 
+
+function setup() { 
+
+canvas = createCanvas(480 , 380) ; 
+canvas.center() ;
+video = createCapture(VIDEO) ; 
+video.size(480 , 380) ;  
+video.hide() ; 
+} 
+
+function draw() { 
+
+image(video , 0 , 0 , 480 , 380) 
+
+} 
+
+function start() { 
+
+objectDetector = ml5.objectDetector('cocossd' , modelLoaded) ; 
+document.getElementById("status").innerHTML = "Detecting objects" ; 
+object_name = document.getElementById("object_name").value ; 
+} 
+
+function modelLoaded() {
+
+console.log("modelLoaded") ;
+status = true ; 
+}
